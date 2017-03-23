504dfdce81c8d24f52aab21ae1d0feaf5f0be3c3
Armando Santos <armandoifsantos@gmail.com>
2017-03-23 16:12:49 +0000

    Renomeação do ficheiro hashtable.h para hashTArt.h (respetiva remoção do ficheiro mal nomeado)

--- hashTArt.h
+++ hashTArt.h
@@ -15,9 +15,10 @@ typedef struct hashtable{
 }*hashTArt [SIZE], artNodo;
 
 
-int hash (char * title_ID);
+int hash (long title_ID);
 void hashTArt_Init (hashTArt h);
-int hashTArt_Add (hashTArt h, char* title, long title_ID, int n_bytes, int n_words, int );
+int hashTArt_Add (hashTArt h, char* title, long title_ID, int n_bytes, int n_words, long revision_id, char* timestamp
+					 char * contributor_name, long contributor_id);
 char hashTArt_GetTitle (hashTArt h, long article_id);
 char** hashTArt_Prefix (hashTArt h, char * prexix);
 char* hashTArt_Timestamp (hashTArt h, long article_id, long revision_id);
