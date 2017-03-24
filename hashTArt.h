aae40bfef5a9a785be3e4b2219534f5f054e102c
Armando Santos <armandoifsantos@gmail.com>
2017-03-24 00:01:42 +0000

    Alteração da estrutura da tabela de hash de artigos (ficheiro hashTArt.h) para receber um 'int heapInd' que representa o indice de um nodo na MaxHeap de artigos. 	Adicionado ficheiro maxHeapArt.h, que possui as funções que operam sobre a max heap de artigos.

--- hashTArt.h
+++ hashTArt.h
@@ -3,22 +3,22 @@
 #include <stdio.h>
 #include "linkedListRevisions.h"
 
-#define SIZE 544516634
+#define SIZE 19889 // Número primo mais baixo e mais próximo de 19886, número de revisões únicas
 
 typedef struct hashtable{
 	char * title;
 	long title_ID;
 	int n_bytes;
 	int n_words;
+	int heapInd; // -1 se não estiver na heap;
 	struct revisionsList * revisions;
 	struct hashtable * next;	
-}*hashTArt [SIZE], artNodo;
+}*hashTArt[SIZE], artNodo;
 
 
 int hash (long title_ID);
 void hashTArt_Init (hashTArt h);
-int hashTArt_Add (hashTArt h, char* title, long title_ID, int n_bytes, int n_words, long revision_id, char* timestamp
-					 char * contributor_name, long contributor_id);
+int hashTArt_Add (hashTArt h, char* title, long title_ID, int n_bytes, int n_words, long revision_id, char* timestamp, char * contributor_name, long contributor_id);
 char hashTArt_GetTitle (hashTArt h, long article_id);
 char** hashTArt_Prefix (hashTArt h, char * prexix);
 char* hashTArt_Timestamp (hashTArt h, long article_id, long revision_id);
---
+++ maxHeapArt.h
@@ -0,0 +1,15 @@
+#include "hashTArt.h"
+
+#define PARENT(i) (i-1)/2  // os indices do array começam em 0
+#define LEFT(i) 2*i + 1
+#define RIGHT(i) 2*i + 2
+
+typedef struct maxheapart{
+	int used;
+	hashTArt heap;
+} *maxHeapArt;
+
+void maxHeapArt_Init(maxHeapArt mh);
+int maxHeapArt_Insert(maxHeapArt mh, artNodo* node);
+long* maxHeapArt_Top20(maxHeapArt mh); // Querie 6
+long* maxHeapArt_TopN(maxHeapArt mh); // Querie 8
