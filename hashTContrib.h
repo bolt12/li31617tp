4dcfef750ba5c8fbdf3e33882d2075ef1b86cb21
Armando Santos <armandoifsantos@gmail.com>
2017-05-01 02:21:22 +0000

    - Adicionada documentação das AVL's 	- Pequenas alterações no codigo (remoção de variaveis e funções 			nao utilizadas.

--- hashTContrib.h
+++ hashTContrib.h
@@ -6,21 +6,20 @@
 #define SIZE 19889
 
 typedef struct hashtablecontrib{
-	char* contributor_name;
-	long contributor_id;
-	int contributions_number;
+	char* contributor_name;		/**<! Nome do contribuidor */
+	long contributor_id;		/**<! ID do contribuidor */
+	int contributions_number;	/**<! Número de contribuições */
 	struct hashtablecontrib *next;
 } *Contrib, *hashTContrib[SIZE] ;
 
-typedef struct avl *avlContrib;
 /**
-*@brief Inicializa a Tabela de Hash
+*\brief Inicializa a Tabela de Hash
 *@param h Tabela de hash
 * 
 */
 void hashTContribInit(hashTContrib ht);
 /**
-*@brief Insere informação de um contribuidor na tabela de Hash
+*\brief Insere informação de um contribuidor na tabela de Hash
 		 ou atualiza número das suas contribuições caso já exista.
 *@param ht Tabela de hash
 *@param contributor_name Nome do contribuidor
@@ -28,13 +27,13 @@ void hashTContribInit(hashTContrib ht);
 */
 int hashTContribAdd(hashTContrib ht, char* contributor_name, long contributor_id);
 /**
-*@brief Devolve o hashCode com base no ID de contribuidor
+*\brief Devolve o hashCode com base no ID de contribuidor
 *@param contributor_id ID do contribuidor
 *@return hashCode
 */
 int hashTContribHash(long contributor_id);
 /**
-*@brief Procura o nome de um contribuidor com base no seu ID
+*\brief Procura o nome de um contribuidor com base no seu ID
 *@param ht Tabela de Hash
 *@param contributor_id ID do contribuidor
 *@return String com o nome
@@ -42,7 +41,7 @@ int hashTContribHash(long contributor_id);
 */
 char* hashTContribRetrieveName(hashTContrib ht, long contributor_id);
 /**
-*@brief Devolve o número de contribuições de um determinado contribuidor com base no seu ID
+*\brief Devolve o número de contribuições de um determinado contribuidor com base no seu ID
 *@param ht Tabela de Hash
 *@param contributor_id ID do contribuidor
 *@return Número de contribuições
@@ -50,18 +49,18 @@ char* hashTContribRetrieveName(hashTContrib ht, long contributor_id);
 */
 int hashTContribRetrieveContributions(hashTContrib ht, long contributor_id);
 /**
-*@brief Limpa a tabela de Hash
+*\brief Limpa a tabela de Hash
 *@param ht Tabela de Hash
 */
 void hashTContribClean(hashTContrib ht);
 /**
-*@brief Coloca numa lista ligada os nodos com os 10 contribuidores com mais contribuições
+*\brief Coloca numa lista ligada os nodos com os 10 contribuidores com mais contribuições
 *@param[in] ht Tabela de Hash
 *@param[out] list Lista ligada
 */
 void getTop10NodesC(hashTContrib ht, LinkedList* list);
 /**
-*@brief Converte a lista ligada num array com o ID dos 10 contribuidores com mais contribuições
+*\brief Converte a lista ligada num array com o ID dos 10 contribuidores com mais contribuições
 *@param list Lista ligada
 *@return Array com ID dos top10 contribuidores
 */
